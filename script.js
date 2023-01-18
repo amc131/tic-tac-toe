1e1f9c6ccdbf7a507146b20df2e31495733181ce
amc131 <76466256+amc131@users.noreply.github.com>
2023-01-18 13:28:25 -0800

    Initial function to add markers to board

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function clickLogic() {
+  const markerX = '&#10060';
+  const markerCircle = '&#11093';
+  const gameTiles = document.querySelectorAll('.game-tile');
+  for (let i = 0; i < gameTiles.length; i++) {
+    gameTiles[i].addEventListener('click', () => {
+      gameTiles[i].innerHTML = markerCircle;
+    });
+  }
+}
+
 function createBoard() {
   const gameBoardContainer = document.querySelector('.game-board-container');
   for (let i = 0; i < 9; i++) {
@@ -5,6 +16,7 @@ function createBoard() {
     gameTile.classList.add('game-tile');
     gameBoardContainer.appendChild(gameTile);
   }
+  clickLogic();
 }
 
 window.onload = createBoard;
